8abba73d9263bc1ace40f893980cbe839cdae680
AABryson <144609457+AABryson@users.noreply.github.com>
2024-07-29 19:02:17 -0700

    backend

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+
+const app = require("./app");
+// // const createTables = require('./createTables')
+
+// // createTables()
+// // console.log('tables created')
+
+app.listen(3001, () => {
+    console.log('backend listening on port 3001')
+})
+// index.js
+// import http from 'http';
+
+// Create a server object
+// const server = app.createServer((req, res) => {
+// 	// Set the response header
+// 	res.writeHead(200, {'Content-Type': 'text/plain'});
+// 	// Write some text to the response
+// 	res.end('Welcome to my simple Node.js app!');
+// });
+
+// // Define the port to listen on
+// const port = 3001;
+
+// // Start the server
+// server.listen(port, () => {
+// 	console.log(`Server is running on http://localhost:${port}`);
+// });
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,9 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+//deployed using vercel
+//oauth-practice-frontend-h3w5lwich-anthony-brysons-projects.vercel.app
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId='317428792953-lhgk1b018qbomhpfq4cbmu1u7aaujiv1.apps.googleusercontent.com'>
