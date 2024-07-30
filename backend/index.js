565f67b865e30c05c0d1c3b3bcfa8a2922dea028
AABryson <144609457+AABryson@users.noreply.github.com>
2024-07-30 11:55:16 -0700

    cors6

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,18 @@
 //     console.log('backend listening on port 3001')
 // })
 // index.js
+const express = require('express');
+const app = express();
 const http = require('http');
+const cors = require('cors');
+app.use(cors())
 
 // Create a server object
 const server = http.createServer((req, res) => {
 	// Set the response header
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	// Write some text to the response
-	res.end('Welcome to my simple Node.js app!');
+	res.end('Welcome!');
 });
 
 // Define the port to listen on
